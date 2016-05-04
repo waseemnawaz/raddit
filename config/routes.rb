173c9149e381797206c4e29256a410e5458f9097
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2016-05-04 13:31:42 +0200

    add comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :links do
     member do
-      put "like", to: "links#upvote"
+      put "like",    to: "links#upvote"
       put "dislike", to: "links#downvote"
     end
+    resources :comments
   end
 
   root to: "links#index"
